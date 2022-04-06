68257956d456bce535b645234058cca36688d902
Laura Ciaran <lauraciaranalfano@gmail.com>
2022-04-06 19:02:46 +0200

    Add files via upload

--- nice_code.py
+++ nice_code.py
@@ -17,14 +17,16 @@ import math
 from Bio.PDB.Polypeptide import three_to_one
 from Bio.PDB.Polypeptide import Polypeptide
 import flexcalc
+from statistics import mean, stdev
+import dictionaries
 
 
 def uniprot_to_pdb(query_ID):
     """ Funtion that obtains the fasta file from Uniprot using the Uniprot ID"""
     url_link = 'https://www.uniprot.org/uniprot/' + query_ID + '.fasta'
-    fasta_file = wget.download(url_link)
+    wget.download(url_link)
 
-## uniprot_to_pdb("P06401")
+uniprot_to_pdb("P06401")
 
 def query_info_from_fasta(fasta_file):
  """ Function that returns the query information, ID and sequence,
@@ -45,11 +47,11 @@ def top_10_blast_idlist(fasta_file):
     stdt, stdr= cline()
 
     ## getting the 10 best porteins IDs
-    with open ("results.out", "r") as file:
+    with open ("blast_results.out", "r") as file:
         list_IDs = []
         for line in file:
             ID = line[4:8]
-            if len(list_IDs) < 2:
+            if len(list_IDs) < 5:
                 if ID not in list_IDs:
                     list_IDs.append(ID)
     return (list_IDs)
@@ -65,7 +67,7 @@ def homologous_PDB(list_hom, query):
         file.write(str(">" + query[0] + "\n" + query[1] +"\n"))
         for Id in list_hom:
             url_pdb = 'https://files.rcsb.org/view/' + Id + '.pdb'
-            pdb_file = wget.download(url_pdb)
+            wget.download(url_pdb)
             PDB_file_path = Id + '.pdb'
             query_seqres = SeqIO.parse(PDB_file_path, 'pdb-atom')
             query_chain_id = Id.upper() + ':A'
@@ -80,11 +82,11 @@ def clustalw(aln_file = "aln_input.fa"):
     """Function that performs the ClustalW alignment and converts it to fasta
     format, no input needed"""
     ## performing the clustalw
-    cmd = ClustalwCommandline("clustalw", infile=aln_file, outfile="output.aln")
+    cmd = ClustalwCommandline("clustalw", infile=aln_file, outfile="aln_output.aln")
     stdout, stderr = cmd()
     ## converting it to fasta format
-    align = AlignIO.read("output.aln", "clustal")
-    count = SeqIO.write(align, "output.fa", "fasta")
+    align = AlignIO.read("aln_output.aln", "clustal")
+    SeqIO.write(align, "aln_output.fa", "fasta")
 
 clustalw()
 
@@ -105,15 +107,34 @@ def pdb_bfactor_info(list_hom):
                     bfactor = float(line[61:66])
                     if 'A' in chain:
                         if 'CA' in atom:
-                            location += 1
-                            residue = three_to_one(residue)
-                            pdb_data.setdefault(Id, {}).setdefault(location, {}).setdefault(residue, bfactor)
+                            if line[16] != "B":
+                                location += 1
+                                residue = three_to_one(residue)
+                                pdb_data.setdefault(Id, {}).setdefault(location, {}).setdefault(residue, bfactor)
     return(pdb_data)
 
 dic_pdb_data= pdb_bfactor_info(list)
-## print (dic_pdb_data)
+##print (dic_pdb_data)
 
-def alignment_to_dict(alignment_fasta = "output.fa"):
+def pdb_bfactor_info_normalized(pdb_data_dict):
+    """Function that normalises the b-factors of the PDB and returns the
+    modified dictionary"""
+    for id in pdb_data_dict:
+        list_bfactors = []
+        for pos in pdb_data_dict[id]:
+            for aa, bfactor in pdb_data_dict[id][pos].items():
+                list_bfactors.append(bfactor)
+        mn = mean(list_bfactors)
+        std = stdev(list_bfactors)
+        for pos in pdb_data_dict[id]:
+            for aa, bfactor in pdb_data_dict[id][pos].items():
+                pdb_data_dict[id][pos][aa] = abs((bfactor - mn) / std)
+    return (pdb_data_dict)
+
+dic_pdb_data = pdb_bfactor_info_normalized(dic_pdb_data)
+##print(dic_pdb_data)
+
+def alignment_to_dict(alignment_fasta = "aln_output.fa"):
     """Function that returns a dictionary with the postions of each aminoacid or
     gap of each protein in the fasta alignment file"""
     msa_dict = {}
@@ -134,32 +155,37 @@ def b_factor_dictionary(aln_dict, PDB_dict, query):
     from the homologues"""
     ## creating the b_factor dictionary
     b_factor_dict = {}
-    for id, seq in aln_dict.items():
+    for id, pos in aln_dict.items():
         if id == query[0]:
-            bf_list = []
-            for id2, seq2 in aln_dict.items():
+            for id2, pos2 in aln_dict.items():
                 aln_counter = 0
                 pdb_loc = 1
                 if id2 != id :
-                    for aa in seq:
-                        if seq[aln_counter] == seq2[aln_counter]:
-                            aa = seq2[aln_counter]
-                            bf_pdb = PDB_dict[id2][pdb_loc][aa]
-                            b_factor_dict.setdefault(aln_counter, {}).setdefault(aa, []).append(bf_pdb)
+                    for aa in pos2:
+                        if pos[aln_counter] == pos2[aln_counter]:
+                            aminoacid = pos2[aln_counter]
+                            bf_pdb = PDB_dict[id2][pdb_loc][aminoacid]
+                            b_factor_dict.setdefault(aln_counter, {}).setdefault(aminoacid, []).append(bf_pdb)
                             pdb_loc = pdb_loc + 1
                         aln_counter += 1
     ## calculating the means for each position
     with open ("predicted_bfactors.txt", "w") as file:
         i = 1
-        file.write(str("Position"+"\t"+"Aminoacid"+"\t"+"B-factor"+"\n"))
+        file.write(str("Position"+"\t"+"Aminoacid"+"\t"+"B-factor"+"\t"+"Type"+"\n"))
         while (i < len(query[1])):
-            if i in b_factor_dict.keys():
+            if i in b_factor_dict:
                 for aa, list in b_factor_dict[i].items():
                     b_factor= round(sum(list) / len(list), 2)
-                    file.write(str(str(i)+"\t"+aa+"\t"+str(b_factor)+"\n"))
+                    if aa in dictionaries.rigid:
+                        file.write(str(str(i)+"\t"+aa+"\t"+str(b_factor)+"\t"+"R"+"\n"))
+                    else:
+                        file.write(str(str(i)+"\t"+aa+"\t"+str(b_factor)+"\t"+"F"+"\n"))
             else:
                 b_factor = flexcalc.flexcalc(query[1], i-1)
-                file.write(str(str(i)+"\t"+query[1][i-1]+"\t"+str(b_factor)+"\n"))
+                if query[1][i-1] in dictionaries.rigid:
+                    file.write(str(str(i)+"\t"+query[1][i-1]+"\t"+str(b_factor)+"\t"+"R"+"\n"))
+                else:
+                    file.write(str(str(i)+"\t"+query[1][i-1]+"\t"+str(b_factor)+"\t"+"F"+"\n"))
             i += 1
 
 b_factor_dictionary(dic_msa, dic_pdb_data, query)
